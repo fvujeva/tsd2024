9a2daa74ab2ca2f6e86a95fbc2799f3e00957393
Filip Vujeva <filip.vujeva123@gmail.com>
2025-03-24 17:26:53 +0100

    satisfactory task

--- 03-LINQ/GoldSavings.App/Program.cs
+++ 03-LINQ/GoldSavings.App/Program.cs
@@ -19,5 +19,67 @@ class Program
             Console.WriteLine($"The price for {goldPrice.Date} is {goldPrice.Price}");
         }
 
+        Console.WriteLine("### Satisfactory task ###");
+        
+        Console.WriteLine("Task 1: Leap Year");
+        Func<int, bool> isLeap = x => (x % 4 == 0 && x % 100 != 0) || (x % 400 == 0);
+        var years = Enumerable.Range(1900, 125);
+        foreach (var year in years)
+        {
+            Console.WriteLine($"Year: {year} is leap year: {isLeap(year)}");
+        }
+        
+        RandomList<int> randomList = new RandomList<int>();
+
+        randomList.Add(1);
+        randomList.Add(2);
+        randomList.Add(3);
+        randomList.Add(4);
+
+        Console.WriteLine($"Random Get (Index 2): {randomList.Get(2)}");
+        Console.WriteLine($"Is Empty? {randomList.IsEmpty()}");
+    }
+
+    class RandomList<T>
+    {
+        private List<T> items;
+        private Random random;
+
+        public RandomList()
+        {
+            items = new List<T>();
+            random = new Random();
+        }
+
+        public void Add(T element)
+        {
+            if (random.Next(2) == 0)
+            {
+                items.Insert(0, element);
+            }
+            else
+            {
+                items.Add(element);
+            }
+        }
+
+        public T Get(int index)
+        {
+            if (items.Count == 0)
+                throw new InvalidOperationException("The list is empty.");
+
+            if (index >= items.Count)
+            {
+                index = items.Count - 1;
+            }
+
+            int randomIndex = random.Next(0, index + 1);
+            return items[randomIndex];
+        }
+
+        public bool IsEmpty()
+        {
+            return items.Count == 0;
+        }
     }
 }
